512bbb9f231c163fc18e7c031df0b7dcf9ed99d9
kogaAkcs <gray9yuki@gmail.com>
2020-11-02 11:02:26 +0900

    101102koga02

---
+++ src/OSSmainPackage/teacher.java
@@ -0,0 +1,14 @@
+package OSSmainPackage;
+
+import java.io.Serializable;
+
+public class teacher implements Serializable {
+
+	int teacherID;
+	String password;
+	String maliaddress
+	char name;
+	
+	public
+
+}
